5307260cf166073642ab786741b4a2755b35ccb0
Muhammed Irfan S <irfanmhdm123@gmail.com>
2026-07-12 05:15:41 +0530

    Completed View

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import heroImg from './assets/hero.png'
 import './App.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import Addcourse from './components/Addcourse'
+import View from './components/View'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -14,6 +15,7 @@ function App() {
       <BrowserRouter>
       <Routes>
         <Route path = "/add" element = {<Addcourse/>} />
+        <Route path = "/view" element = {<View/>} />
       </Routes>
       </BrowserRouter>
     </>
